13215c9a66c822ec95677a2d5f244a268c936131
James <jaeco.89@gmail.com>
2015-04-13 16:48:53 -0600

    Basic Layout
    Setting up layout and design of the website pre-validation

--- HelloMVC/Controllers/HomeController.cs
+++ HelloMVC/Controllers/HomeController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HelloMVC.Models;
 
 namespace HelloMVC.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+        
+        public ViewResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult Confirmation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult SubmissionConfirmation(ContactData contactData)
+        {
+            return View("Test", contactData);
         }
     }
 }
